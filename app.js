2cdceaec966a1e8dcc57b6e5426ecff33f6cbe62
pbchun <pbchun12@gmail.com>
2018-06-27 13:17:21 -0600

    removed cors from app.js and added to newEvent.js in routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 
 const event = require("./routes/newEvent");
