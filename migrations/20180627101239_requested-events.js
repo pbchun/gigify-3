82e9d5e07cadec1ee38c011c2244745d30e598a8
pbchun <pbchun12@gmail.com>
2018-06-27 11:35:35 -0600

    corrected artistName to artist and added image section

--- migrations/20180627101239_requested-events.js
+++ migrations/20180627101239_requested-events.js
@@ -3,12 +3,13 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('event', table => {
         table.increments();
         table.text('date');
-        table.text('artistName');
+        table.text('artist');
         table.text('contactName');
         table.text('phone');
         table.text('email');
         table.text('website');
         table.text('musicSample');
+        table.text('image');
         table.text('description');
     })
 };
--- request-form.js
+++ request-form.js
@@ -1,31 +1,34 @@
 module.exports = [{
     id: 1,
     date: "06-30-2018",
-    artistName: "Hi-Dive",
+    artist: "Hi-Dive",
     contactName: "John Johnson",
     phone: "333-555-8888",
     email: "john@galvanize.com",
     website: "johnjohnson.com",
     musicSample: "www.youtube.com/johnj",
+    image: "image1.jpg",
     description: "Not Jack Johnson"
 }, {
     id: 2,
     date: "07-10-2018",
-    artistName: "Hi-Dive",
+    artist: "Hi-Dive",
     contactName: "Tyler Swift",
     phone: "222-444-9999",
     email: "tyler@galvanize.com",
     website: "tylerswift.com",
     musicSample: "www.youtube.com/tylers",
+    image: "image2.jpg",
     description: "Not Taylor Swift"    
 }, {
     id: 3,
     date: "07-20-2018",
-    artistName: "Hi-Dive",
+    artist: "Hi-Dive",
     contactName: "Jimmy Luffet",
     phone: "111-444-5555",
     email: "jimmy@galvanize.com",
     website: "jimmyluffet.com",
     musicSample: "www.youtube.com/jimmyl",
+    image: "image3.jpg",
     description: "Not Jimmy Buffet"
 }]
