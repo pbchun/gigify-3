fdd6895a6bad4d1d7fc3fc17b7cc90f02b4b9d15
pbchun <pbchun12@gmail.com>
2018-06-28 14:17:18 -0600

    updated request form artists with local bands

--- request-form.js
+++ request-form.js
@@ -1,34 +1,34 @@
 module.exports = [{
     id: 1,
     date: "06-30-2018",
-    artist: "John Johnson",
-    contactName: "John Johnson",
+    artist: "RUMTUM",
+    contactName: "John Hastings",
     phone: "333-555-8888",
     email: "john@galvanize.com",
-    website: "johnjohnson.com",
-    musicSample: "www.youtube.com/johnj",
-    image: "https://fastercapital.com/images/people/john_johnson.jpg",
-    description: "Not Jack Johnson"
+    website: "https://rumtum.bandcamp.com/",
+    musicSample: "https://www.youtube.com/watch?v=lecOlyInoRs&list=PLaxvvhrY8jkLAULUOPYYz8sleJ6oQzNSF&index=1",
+    image: "https://bandcampblog.files.wordpress.com/2016/11/rumtum-600-2.jpg?w=800",
+    description: "Electronic producer and instrumental enthusiast has a unique approach to crafting his melodic melodies."
 }, {
     id: 2,
     date: "07-10-2018",
-    artist: "Tyler Swift",
-    contactName: "Tyler Swift",
+    artist: "Allout Helter",
+    contactName: "Ross Hostage",
     phone: "222-444-9999",
-    email: "tyler@galvanize.com",
-    website: "tylerswift.com",
-    musicSample: "www.youtube.com/tylers",
-    image: "https://s3-us-west-1.amazonaws.com/co-directory-images/tylerjswift.jpg",
-    description: "Not Taylor Swift"    
+    email: "ross@galvanize.com",
+    website: "https://allouthelter.bandcamp.com/",
+    musicSample: "https://www.youtube.com/watch?v=XWch0u1UUkU&list=PLwWKX-BtZkJm2X_-vOs4eb0YyGxDBN455&index=1",
+    image: "https://images1.westword.com/imager/allout-helter-on-the-value-of-riot-fest-to/u/original/6415272/allouthelter_jesusverduzco_opt.jpg",
+    description: "Allout Helter is always busy annihilating whatever stage they’re on with their intense and engaging live show, having even worked their way to festivals like Riot Fest Denver and The Fest."    
 }, {
     id: 3,
     date: "07-20-2018",
-    artist: "John Bayer",
-    contactName: "John Bayer",
+    artist: "The Windermeres",
+    contactName: "Chris Windermere",
     phone: "111-444-5555",
-    email: "johnb@galvanize.com",
-    website: "johnbayer.com",
-    musicSample: "www.youtube.com/johnb",
-    image: "http://www.bayersolicitors.co.uk/images/bayer_staff_photo_john_bayer.jpg",
-    description: "Not John Mayer"
+    email: "chris@galvanize.com",
+    website: "https://thewindermeres.bandcamp.com/",
+    musicSample: "https://www.youtube.com/watch?v=UtsozPwNrOE&list=PLjN3iiRmj4bLTBhpLkJky7FtxaFOWXa7r&index=1",
+    image: "https://images1.westword.com/imager/u/original/8094718/dsc_7646_the_windermeres_by_patrick_houdek.jpg",
+    description: "The Windermeres are a punk rock band from Denver, Colorado. They formed in December of 2012. They named their band after a street in their home town."
 }]
